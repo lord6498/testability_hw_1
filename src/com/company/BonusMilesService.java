20b3efeb5425330892f43e802cb56852cb8e9022
Nick <>
2021-01-23 11:14:21 +0300

    homework

---
+++ src/com/company/BonusMilesService.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class BonusMilesService {
+    public int calculate(int cost) {
+
+
+        {
+            int price = cost;
+            int miles = price / 20;
+            return miles;
+        }
+
+
+    }
+}
